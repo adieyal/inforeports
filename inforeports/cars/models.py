7885c47c310f9f8cfab79b06d472d6e46780014a
Adi Eyal <adi@burgercom.co.za>
2013-01-26 13:39:40 +0200

    Added model to store car models

--- inforeports/cars/models.py
+++ inforeports/cars/models.py
@@ -4,7 +4,7 @@ class CarAd(models.Model):
     title = models.CharField(max_length=100)
     date_listed = models.DateField()
     price = models.FloatField()
-    address = models.TextField()
+    address = models.TextField(blank=True)
     seller_type = models.CharField(max_length=20)
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
@@ -13,7 +13,7 @@ class CarAd(models.Model):
     mileage = models.IntegerField()
     transmission = models.CharField(max_length=20, blank=True)
     drive_train = models.CharField(max_length=40, blank=True)
-    air_conditioning = models.CharField(max_length=20)
+    air_conditioning = models.CharField(max_length=20, blank=True)
     description = models.TextField()
     province = models.CharField(max_length=40)
     area = models.CharField(max_length=20)
@@ -22,3 +22,10 @@ class CarAd(models.Model):
 
     def __unicode__(self):
         return u'%s' % (self.title)
+
+class CarModel(models.Model):
+    make = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return u'%s - %s' % (self.make, self.model)
